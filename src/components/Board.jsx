8a56dc77420dfd8e918c20713be124099ce49930
Suman <dhalisuman001@gmail.com>
2022-08-09 16:29:38 +0530

    react states and event

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square.jsx';
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(false);
+
+  const squareClick = loc => {
+    if (board[loc]) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, location) => {
+        if (location == loc) {
+          return isNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+
+    setIsNext(prev => !prev);
+  };
+
+  const returnSquare = loc => {
+    return (
+      <Square
+        value={board[loc]}
+        onClick={() => {
+          squareClick(loc);
+        }}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {returnSquare(0)}
+        {returnSquare(1)}
+        {returnSquare(2)}
       </div>
 
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {returnSquare(3)}
+        {returnSquare(4)}
+        {returnSquare(5)}
       </div>
 
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {returnSquare(6)}
+        {returnSquare(7)}
+        {returnSquare(8)}
       </div>
     </div>
   );
