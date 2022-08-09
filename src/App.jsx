ed5db53a282c851012e9eb5a74a36480eb48543d
Suman <dhalisuman001@gmail.com>
2022-08-09 15:56:19 +0530

    Css complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-
+import React from 'react';
+import Board from './components/Board';
 export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
+  <div className="app">
+    <h1>Tic Tac Tow</h1>
+    <Board />
+  </div>
 );
